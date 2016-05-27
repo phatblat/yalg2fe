2a2a46dd9a8ff606d661cede5f32410770edce08
Ben Chatelain <ben@octop.ad>
2016-05-26 20:37:35 -0600

    Add iOS framework project

---
+++ yalg2fe/yalg2fe.h
@@ -0,0 +1,19 @@
+//
+//  yalg2fe.h
+//  yalg2fe
+//
+//  Created by Ben Chatelain on 5/26/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for yalg2fe.
+FOUNDATION_EXPORT double yalg2feVersionNumber;
+
+//! Project version string for yalg2fe.
+FOUNDATION_EXPORT const unsigned char yalg2feVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <yalg2fe/PublicHeader.h>
+
+
